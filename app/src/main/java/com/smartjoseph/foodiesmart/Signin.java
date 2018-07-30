4fb8f631a57ba5b3c92bd1137ffe66dc2f146646
Smart Joseph <iamsmartjoseph@gmail.com>
2018-07-30 22:42:01 +0100

    Initial commit

---
+++ app/src/main/java/com/smartjoseph/foodiesmart/Signin.java
@@ -0,0 +1,15 @@
+package com.smartjoseph.foodiesmart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+    }
+}
